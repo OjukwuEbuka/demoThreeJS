210438824d0ca9f49ad69b422ab7bbc0069f7999
Ojukwu Emmanuel <ebukaojukwu23@gmail.com>
2021-05-16 16:01:54 +0100

    Removed unused var

--- src/components/EditModal.tsx
+++ src/components/EditModal.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import { Divider, Box, InputLabel, Button } from '@material-ui/core';
